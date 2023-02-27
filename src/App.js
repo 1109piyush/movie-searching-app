8b8d55278e7403a0d14cf4984c59121678a7c939
1109piyush <piyushsinghal11091999@gmail.com>
2023-02-27 22:53:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const fetchRandomMovies = async () => {
+    const response = await axios.get(`http://www.omdbapi.com/?s=random&apikey=263d22d8`);
+    setMovies(response.data.Search);
+  };
+
+  useEffect(() => {
+    fetchRandomMovies();
+  }, []);
+
+  const searchMovies = async (event) => {
+    event.preventDefault();
+    const response = await axios.get(`http://www.omdbapi.com/?s=${searchQuery}&apikey=263d22d8`);
+    setMovies(response.data.Search);
+  };
+
+  const getMovieDetails = async (imdbID) => {
+    const response = await axios.get(`http://www.omdbapi.com/?i=${imdbID}&apikey=263d22d8`);
+    setSelectedMovie(response.data);
+  };
+
+  const goHome = () => {
+    setSelectedMovie(null);
+    setSearchQuery('');
+    fetchRandomMovies();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 onClick={goHome}>Movies</h1>
       </header>
+      {selectedMovie ? (
+        <div className="selected-movie-container">
+          <div className="selected-movie-details">
+            <h2>{selectedMovie.Title}</h2>
+            <img src={selectedMovie.Poster} alt={selectedMovie.Title} />
+            <p><strong>Genre:</strong> {selectedMovie.Genre}</p>
+            <p><strong>Released:</strong> {selectedMovie.Released}</p>
+            <p><strong>Runtime:</strong> {selectedMovie.Runtime}</p>
+            <p><strong>Director:</strong> {selectedMovie.Director}</p>
+            <p><strong>Actors:</strong> {selectedMovie.Actors}</p>
+            <p><strong>Plot:</strong> {selectedMovie.Plot}</p>
+          </div>
+        </div>
+      ) : (
+        <>
+          <form onSubmit={searchMovies}>
+            <input type="text" value={searchQuery} onChange={(event) => setSearchQuery(event.target.value)} placeholder="Search for movies" />
+            <button type="submit">Search</button>
+          </form>
+          <div className="movie-list">
+            {movies.map((movie) => (
+              <div className="movie-item" key={movie.imdbID} onClick={() => getMovieDetails(movie.imdbID)}>
+                <img src={movie.Poster} alt={movie.Title} />
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
